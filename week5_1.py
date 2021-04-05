ab2565ecfe10849a9d44753e92aacd91d2164882
CHO1116 <dudwnscho@naver.com>
2021-04-05 12:07:49 +0900

    week5

--- week5_1.py
+++ week5_1.py
@@ -1,6 +1,8 @@
+# 함수를 활용하여 BMI지수 측정하기
 def Bmi_Calculator(Height, Weight):
-    bmi = Weight / Height ** 2
+    bmi = Weight / Height ** 2  # 제곱표현법
     return bmi
+
 h = float(input("키를 입력해주세요(단위: m)\n"))
 w = float(input("체중을 입력해주세요(단위: kg\n"))
-print(f"your bmi is {Bmi_Calculator(h, w):.2f}")
+print(f"your bmi is {Bmi_Calculator(h, w):.2f}")    # 소수점 아래 자리수 표현법
--- week5_3.py
+++ week5_3.py
@@ -1,6 +1,7 @@
+# 함수를 활용하여 구구단 출력하기
 def Multiplication_table(number):
     print("[구구단", number, "단]")
-    for i in range(1,10):
+    for i in range(1, 10):
         print(number, "*", i, "=", number * i)
 
 g = int(input("입력하신 숫자의 구구단을 출력합니다. "))
